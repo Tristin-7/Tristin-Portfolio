5b54c9adcccf610e565d436efd9dd6a8ec9d205f
Tristin Van Der Lingen <vanderlingentristin3@gmail.com>
2025-09-18 12:10:09 +0000

    the download resume button should download the pdf in the public folder

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -31,7 +31,7 @@ export function SiteHeader() {
         
         <div className="hidden md:flex flex-1 items-center justify-end space-x-4">
           <Button asChild>
-            <a href="/resume.pdf" download="resume.pdf">Download Resume</a>
+            <a href="/Tristin Resume.pdf" target="_blank" rel="noopener noreferrer">Download Resume</a>
           </Button>
         </div>
 
@@ -59,7 +59,7 @@ export function SiteHeader() {
                   </SheetClose>
                 ))}
                  <Button asChild className="mt-4">
-                  <a href="/resume.pdf" download="resume.pdf">Download Resume</a>
+                  <a href="/Tristin Resume.pdf" target="_blank" rel="noopener noreferrer">Download Resume</a>
                 </Button>
               </div>
             </SheetContent>
