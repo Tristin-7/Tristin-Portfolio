806ccb4d1e7200f8ed1ad0f4a8befac71bc2c4ca
Tristin Van Der Lingen <vanderlingentristin3@gmail.com>
2025-09-18 11:32:59 +0000

    can you make the email link take you into a gmail composed email with my

--- src/config/site.ts
+++ src/config/site.ts
@@ -12,7 +12,7 @@ export const socialLinks: SocialLink[] = [
   { name: 'GitHub', url: 'https://github.com', icon: Github },
   { name: 'LinkedIn', url: 'https://www.linkedin.com/in/tristin-van-der-lingen-95a067350/', icon: Linkedin },
   { name: 'Twitter', url: 'https://twitter.com', icon: Twitter },
-  { name: 'Email', url: 'mailto:vanderlingentristin3@gmail.com', icon: Mail },
+  { name: 'Email', url: 'https://mail.google.com/mail/?view=cm&fs=1&to=vanderlingentristin3@gmail.com', icon: Mail },
 ];
 
 export const projects: Project[] = [
