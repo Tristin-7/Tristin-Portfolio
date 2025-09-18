94c9fdc01fcb78559e011fbd824b0f34cbd40009
Tristin Van Der Lingen <vanderlingentristin3@gmail.com>
2025-09-18 11:15:35 +0000

    the news sentiment demo should take you to this https://9000-firebase-st

--- src/config/site.ts
+++ src/config/site.ts
@@ -35,7 +35,7 @@ export const projects: Project[] = [
     description: 'A web application that analyzes news headlines and determines the sentiment of the news.',
     imageUrl: '/project 3 Sentiment Navigator.png',
     imageHint: 'news sentiment',
-    demoUrl: '#',
+    demoUrl: 'https://9000-firebase-studio-1756966665623.cluster-etsqrqvqyvd4erxx7qq32imrjk.cloudworkstations.dev',
   },
 ];
 
