4cced23a76a4534da5552d131898fb880b9420b8
Tristin Van Der Lingen <vanderlingentristin3@gmail.com>
2025-09-22 09:01:58 +0000

    the glow should disappear and come back

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ export default {
           },
         },
         'glow': {
-          '0%, 100%': { boxShadow: '0 0 20px hsl(var(--primary))' },
+          '0%, 100%': { boxShadow: '0 0 10px hsl(var(--primary), 0.2)' },
           '50%': { boxShadow: '0 0 35px hsl(var(--primary))' },
         }
       },
