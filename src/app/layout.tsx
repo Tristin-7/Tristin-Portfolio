ce8ca06b14e60cdcd985273f25fbebb69bc9a25c
Tristin Van Der Lingen <vanderlingentristin3@gmail.com>
2025-09-22 08:04:02 +0000

    add a few more xbox inspired things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body bg-secondary antialiased">
+      <body className="font-body antialiased">
         {children}
         <Toaster />
       </body>
