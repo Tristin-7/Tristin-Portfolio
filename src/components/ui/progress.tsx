5c6140aad3b3ec46b0ef618e84d59b1f1c4f915b
Tristin Van Der Lingen <vanderlingentristin3@gmail.com>
2025-09-22 08:46:23 +0000

    any more similar reccomendations

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,7 +12,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      "relative h-4 w-full overflow-hidden rounded-sm bg-secondary border-2 border-primary/50",
       className
     )}
     {...props}
